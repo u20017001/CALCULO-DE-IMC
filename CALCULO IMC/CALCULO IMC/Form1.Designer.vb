1cf96b373d6174cb402b28c3a17689f07a701782
u20017001 <u20017001@continental.edu.pe>
2016-12-11 18:21:47 -0500

    Subida de archivos.
    Subida de la carpeta principal CALCULO DE IMC y todas las sub-carpetas de la aplicación CALCULO DE IMC creado en Visual Studio 2013.

---
+++ CALCULO IMC/CALCULO IMC/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPeso = New System.Windows.Forms.TextBox()
+        Me.txtAltura = New System.Windows.Forms.TextBox()
+        Me.btnCALCULAR = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.triesgo = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "PESO (kg)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(171, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "ALTURA (m)"
+        '
+        'txtPeso
+        '
+        Me.txtPeso.Location = New System.Drawing.Point(26, 58)
+        Me.txtPeso.Name = "txtPeso"
+        Me.txtPeso.Size = New System.Drawing.Size(97, 20)
+        Me.txtPeso.TabIndex = 2
+        Me.txtPeso.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtAltura
+        '
+        Me.txtAltura.Location = New System.Drawing.Point(163, 58)
+        Me.txtAltura.Name = "txtAltura"
+        Me.txtAltura.Size = New System.Drawing.Size(97, 20)
+        Me.txtAltura.TabIndex = 3
+        Me.txtAltura.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnCALCULAR
+        '
+        Me.btnCALCULAR.Location = New System.Drawing.Point(65, 97)
+        Me.btnCALCULAR.Name = "btnCALCULAR"
+        Me.btnCALCULAR.Size = New System.Drawing.Size(160, 32)
+        Me.btnCALCULAR.TabIndex = 4
+        Me.btnCALCULAR.Text = "CALCULAR"
+        Me.btnCALCULAR.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(120, 220)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(76, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "LIMPIAR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(211, 220)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(64, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "SALIR"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'lblResultado
+        '
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResultado.Location = New System.Drawing.Point(127, 144)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(41, 20)
+        Me.lblResultado.TabIndex = 7
+        Me.lblResultado.Text = "IMC"
+        '
+        'triesgo
+        '
+        Me.triesgo.AutoSize = True
+        Me.triesgo.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.triesgo.Location = New System.Drawing.Point(148, 180)
+        Me.triesgo.Name = "triesgo"
+        Me.triesgo.Size = New System.Drawing.Size(58, 18)
+        Me.triesgo.TabIndex = 8
+        Me.triesgo.Text = ".........."
+        Me.triesgo.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(16, 182)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(125, 15)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "TIPO DE RIESGO :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(301, 255)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.triesgo)
+        Me.Controls.Add(Me.lblResultado)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnCALCULAR)
+        Me.Controls.Add(Me.txtAltura)
+        Me.Controls.Add(Me.txtPeso)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Calculo IMC"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtPeso As System.Windows.Forms.TextBox
+    Friend WithEvents txtAltura As System.Windows.Forms.TextBox
+    Friend WithEvents btnCALCULAR As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents lblResultado As System.Windows.Forms.Label
+    Friend WithEvents triesgo As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ CALCULO IMC/CALCULO IMC/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Dim imc As Double
+
+    Private Sub lblResultado_Click(sender As Object, e As EventArgs) Handles lblResultado.Click
+
+    End Sub
+
+    Private Sub btnCALCULAR_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCALCULAR.Click
+        imc = Val(txtPeso.Text) / (Val(txtAltura.Text) * Val(txtAltura.Text))
+        Select Case imc
+            Case Is < 18.49
+                triesgo.Text = ("MUY POCO PESO")
+            Case 18.5 To 24.9
+                triesgo.Text = ("PROMEDIO")
+            Case 25 To 29.9
+                triesgo.Text = ("AUMENTADO")
+            Case 30 To 34.9
+                triesgo.Text = ("MODERADO")
+            Case 35 To 39.9
+                triesgo.Text = ("SEVERO")
+            Case Else
+                triesgo.Text = ("MUY SEVERO")
+        End Select
+
+        lblResultado.Text = Format(imc, "##.##")
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        End
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        txtAltura.Clear()
+        txtPeso.Clear()
+        lblResultado.Text = ""
+        txtPeso.Focus()
+    End Sub
+
+    Private Sub triesgo_Click(sender As Object, e As EventArgs) Handles triesgo.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
